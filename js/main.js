dc95a73cc8814c0bf5901251b79f8b8a5287ac4d
mariajalmeida <mari9d91@stud.kea.dk>
2021-04-26 10:01:31 +0200

    animation set

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-import "../sass/main.scss";
+const span = (text, index) => {
+    const node = document.createElement('span')
+
+    node.innerText = text;
+    node.style.setProperty('--index', index)
+
+    return node;
+}
+
+const byLetter = text => [...text].map(span)
+
+const splitTargets = document.querySelectorAll('[data-split-letters]')
+
+splitTargets.forEach(node => {
+    let nodes = null;
+    nodes = byLetter(node.innerText)
+
+    if (nodes)
+        node.firstChild.replaceWith(...nodes)
+})
